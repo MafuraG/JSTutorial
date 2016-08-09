2fa007a517ff1f2f92c349d0901209f80e9baae2
Gabriel Mafura <mafurag@hotmail.com>
2016-08-09 14:04:22 +0300

    first lesson

---
+++ Hello world/variables.js
@@ -0,0 +1,17 @@
+var number = 43;
+console.log(number);
+var string = "This is a string";
+console.log(string);
+
+var boolean = true;
+console.log(boolean);
+
+/*Javascript variables are NOT typed. This means that the type of a given variable
+depends on whatever is stored in it at a give moment*/
+
+/*primitive types
+*number (no Intergers as such! all numbers are floating point)
+*string (sequence of unicoed characters 16bit)
+*boolean
+*
+*/
